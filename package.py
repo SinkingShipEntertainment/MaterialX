d51108619545192d8138054c9fcf86bcac1031c6
Marcelo Sercheli <msercheli@gmail.com>
2024-06-18 18:07:25 -0400

    New variant for new python version

--- package.py
+++ package.py
@@ -26,6 +26,7 @@ private_build_requires = [
 variants = [
     ["python-3.7"],
     ["python-3.9"],
+    ["python-3.11"],
 ]
 
 # NOTE: Remember to run (after first time cloning): git submodule update --init --recursive
